641dac97a2b7a7f89e43f7f4cee04c4c9ce43e56
Jinshi He <jih064@ucsd.edu>
2022-05-10 16:51:03 -0700

    Update skillDemo1Test.java

--- skillDemo1Test.java
+++ skillDemo1Test.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class skillDemo1Test {
     @Test
     public void addition() {
-        assertEquals(1, skillDemo1.add(1, 1));
+        assertEquals(2, skillDemo1.add(1, 1));
     }
 }
